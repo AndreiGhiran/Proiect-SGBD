59b8ce010b60a05ed50ab521f3b674b042713bef
AndreiG <48528839+AndreiGhiran@users.noreply.github.com>
2019-05-29 21:21:31 +0300

    Update p11.php

--- ce am facut eu la site/p11.php
+++ ce am facut eu la site/p11.php
@@ -11,7 +11,7 @@ function Afiseaza_programri()
 {
 		global $conn;
 		$id = $_SESSION['id']; 
-		$sql = "select (select nume from furnizori where id=id_furnizor)\"Furnizor\",(select tip from servicii where id=id_serviciu)\"Serviciu\",to_char(data_programare,'dd-mm-yy')\"Data Programarii\",to_char(ora_programare,'HH24:MI')\"Ora Programarii\" from programari where id_client = ".$id."order by\"Data Programarii\",\"Ora Programarii\"";
+		$sql = "select id,(select nume from furnizori where id=id_furnizor)\"Furnizor\",(select tip from servicii where id=id_serviciu)\"Serviciu\",to_char(data_programare,'dd-mm-yy')\"Data Programarii\",to_char(ora_programare,'HH24:MI')\"Ora Programarii\",atendance\"Atendance\" from programari where id_client = ".$id."order by\"Data Programarii\",\"Ora Programarii\"";
 	    $stmt = oci_parse($conn,$sql);
 		$r = oci_execute($stmt);
 		if (!$r) {
@@ -38,6 +38,36 @@ function Afiseaza_programri()
     echo "</tr>\n";
 }
 echo "</table>\n";
+}
+
+function Adauga_programare()
+{
+		global $conn;
+$id = $_SESSION['id'];
+$adaug_idm = $_POST['adaug_idm'];
+$adaug_ids = $_POST['adaug_ids'];
+$adaug_data = $_POST['adaug_data'];
+$adaug_ora = $_POST['adaug_ora'];
+	    $sql='BEGIN add_programare(:id,:id_f,:id_s,:data,:ora); commit; END;';
+		$stmt = oci_parse($conn,$sql);
+		oci_bind_by_name($stmt,':id',$id,32);
+		oci_bind_by_name($stmt,':id_f',$adaug_idm,32);
+		oci_bind_by_name($stmt,':id_s',$adaug_ids,32);
+		oci_bind_by_name($stmt,':data',$adaug_data,32);
+		oci_bind_by_name($stmt,':ora',$adaug_ora,32);
+		oci_execute($stmt);
+}
+
+function Sterge_programare()
+{
+		global $conn;
+		//$sterg_idm = $_POST['sterg_idm'];
+		//$sterg_ids = $_POST['sterg_ids'];
+		$sterg_idp = $_POST['sterg_idp'];
+		$sql = 'BEGIN delete_programare(:id); END;';
+		$stmt = oci_parse($conn,$sql);
+		oci_bind_by_name($stmt,':id',$sterg_idp);
+		oci_execute($stmt);
 }
 
 ?>
@@ -93,13 +123,30 @@ echo "</table>\n";
   <input type="submit" name="adaugaprogramare" value="Submit">
 </form>
 
+<?php
+
+if(isset($_POST['adaugaprogramare']))
+{
+	Adauga_programare();
+}
+?>
+
 <h2>Sterge programare </h2>
  <form action="p11.php" method="post">
-  <input type="user" name="sterg_idm" placeholder="Id magazin"><br><br>
-    <input type="user" name="sterg_ids" placeholder="Id serviciu"><br><br>
+ <!-- <input type="user" name="sterg_idm" placeholder="Id magazin"><br><br>
+    <input type="user" name="sterg_ids" placeholder="Id serviciu"><br><br> -->
 	   <input type="user" name="sterg_idp" placeholder="Id programare"><br><br>
-  <input type="submit" name="stergeprogramare" value="Submit">
+  <input type="submit" name="stergeprogramare" value="Stergere">
 </form>
+
+<?php
+
+if(isset($_POST['stergeprogramare']))
+{
+	Sterge_programare();
+}
+?>
+
 <br><br>
  <form action="p11.php" method="post">
   <input type="submit" name="afiseazaprogramari" value="Afiseaza programari">
@@ -125,35 +172,3 @@ if(isset($_POST['afiseazaprogramari']))
 
 
 </html>
-<?php
-
-function Adauga_programare()
-{
-		global $conn;
-$adaug_idm = $_POST['adaug_idm'];
-$adaug_ids = $_POST['adaug_ids'];
-$adaug_data = $_POST['adaug_data'];
-$adaug_ora = $_POST['adaug_ora'];
-	
-}
-
-function Sterge_programare()
-{
-		global $conn;
-$sterg_idm = $_POST['sterg_idm'];
-$sterg_ids = $_POST['sterg_ids'];
-$sterg_idp = $_POST['sterg_idp'];
-
-	
-}
-
-if(isset($_POST['adaugaprogramare']))
-{
-	Adauga_programare();
-}
-
-if(isset($_POST['stergeprogramare']))
-{
-	Sterge_programare();
-}
-?>
