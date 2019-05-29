bd7374a5646994293553670e97f5ebcf798fb2b1
andreicaloian <48419225+andreicaloian@users.noreply.github.com>
2019-05-29 18:13:27 +0300

    Add files via upload

--- ce am facut eu la site/p2.php
+++ ce am facut eu la site/p2.php
@@ -3,93 +3,10 @@ error_reporting(E_ERROR | E_PARSE);
 session_start(); 
         if($_SESSION['user'] != NULL) {
             header('Location: indexx.php');
-        } ?>
+        } 
 		
-
-<!DOCTYPE html>
-<html>
-
-<head>
-<meta charset="UTF-8">
-<link rel="stylesheet" href="styleSheet.css">
-<title>LineUp</title>
-</head>
-
-
-<body>
-
-<main>
-
-
-<input type="checkbox" id="newsButton">
-
-<label for="newsButton">  </label>
-
-
-<nav>
-
-<input type="checkbox" id="menuButton">
-
-<label for="menuButton">  </label>
-
-
-<ul>
-
-<li><a href="index.php"> Acasa</a></li>
-<li><a href="p1.php"> Programari</a></li>
-<li><a href="p2.php"> Cauta</a></li>
-<li><a href="p3.php">Cont</a></li>
-<li><a href="p4.php"> Contact</a></li>
-
-</ul>
-
-
-</nav>
-
-<aside>
-
-<h2>Autentificare </h2>
- <form action="p2.php" method="post">
-  <input type="text" name="login_username" placeholder="Nume"><br>
-  <input type="password" name="login_password" placeholder="Parola"><br><br>
-  <button type="submit" name="login_submit">Log In</button>
-</form> 
-<p><h2>Inregistrare</h2></p>
- <form action="index.php" method="post">
-  <input type="text" name="register_username" placeholder="Nume"><br>
-  <input type="text" name="register_userpname" placeholder="Prenume"><br>
-  <input type="text" name="register_email" placeholder="Telefon"><br>
-  <input type="text" name="register_telefon" placeholder="Email"><br>
-  <input type="password" name="register_password" placeholder="Parola"><br>
-  <input type="password" name="register_passwordd" placeholder="Confirm Parola"><br><br>
-  <button type="submit" name="register_submit">Register</button>
-</form>
-
-</aside>
-
-<section>
-<br><br>
-<form action="p2.php" method="post">
-  <input type="text" name="nume_magazin" placeholder="Numele magazinului"><br>
-  <input type="submit" name="caut_magazin_nume" value="Submit">
-</form> 
-
-<form action="p2.php" method="post">
-  <input type="text" name="id_magazin" placeholder="Id-ul magazinului"><br>
-  <input type="submit" name="caut_magazin_id" value="Submit">
-</form> 
-
-</section>
-
-</main>
-
-</body>
-
-
-</html>
-
- <?php
- $conn = oci_connect('STUDENT','STUDENT','localhost/XE') or die;
+		
+		 $conn = oci_connect('STUDENT','STUDENT','localhost/XE') or die;
 
 function LogIn ()
 {
@@ -153,25 +70,162 @@ function Register()
 function Cautmagazinnume()
 {
 	global $conn;
-	$name = $_POST['register_username'];
-	$sql = 'BEGIN add_client(:username, :userpname, :password, :email, :tel); commit; END;';
+	$nume_magazin = $_POST['nume_magazin'];
+	$sql = "select id,tip from servicii where id=(select id_serviciu from furnizori where nume='".$nume_magazin."' and rownum=1)";
 	$stmt = oci_parse($conn,$sql);
-		oci_bind_by_name($stmt,':username',$name,32);
-		oci_bind_by_name($stmt,':username',$name,32);
-$stmt = oci_parse($conn,$sql);
-
+	$r = oci_execute($stmt);
+	
+	
+	
+	echo "<table border='1'>\n";
+$ncols = oci_num_fields($stmt);
+echo "<tr>\n";
+for ($i = 1; $i <= $ncols; ++$i) {
+    $colname = oci_field_name($stmt, $i);
+    echo "  <th><b>".htmlspecialchars($colname,ENT_QUOTES|ENT_SUBSTITUTE)."</b></th>\n";
 }
-
+echo "</tr>\n";
+ 
+	
+while (($row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
+    echo "<tr>\n";
+    foreach ($row as $item) {
+        echo "<td>";
+        echo $item!==null?htmlspecialchars($item, ENT_QUOTES|ENT_SUBSTITUTE):"&nbsp;";
+        echo "</td>\n";
+    }
+}
+echo "</table>\n";
+}
 function Cautmagazinid()
 {
 	global $conn;
-	$name = $_POST['register_username'];
-	$sql = 'BEGIN add_client(:username, :userpname, :password, :email, :tel); commit; END;';
+	$id_magazin = $_POST['id_magazin'];
+   $sql = "select id,tip from servicii where id=(select id_serviciu from furnizori where id=".$id_magazin." and rownum=1)";
 	$stmt = oci_parse($conn,$sql);
-		oci_bind_by_name($stmt,':username',$name,32);
-$stmt = oci_parse($conn,$sql);
-
+	$r = oci_execute($stmt);
+	
+	
+	
+	echo "<table border='1'>\n";
+$ncols = oci_num_fields($stmt);
+echo "<tr>\n";
+for ($i = 1; $i <= $ncols; ++$i) {
+    $colname = oci_field_name($stmt, $i);
+    echo "  <th><b>".htmlspecialchars($colname,ENT_QUOTES|ENT_SUBSTITUTE)."</b></th>\n";
 }
+echo "</tr>\n";
+ 
+	
+while (($row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
+    echo "<tr>\n";
+    foreach ($row as $item) {
+        echo "<td>";
+        echo $item!==null?htmlspecialchars($item, ENT_QUOTES|ENT_SUBSTITUTE):"&nbsp;";
+        echo "</td>\n";
+    }
+}
+echo "</table>\n";
+}
+
+?>
+		
+
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="styleSheet.css">
+<title>LineUp</title>
+</head>
+
+
+<body>
+
+<main>
+
+
+<input type="checkbox" id="newsButton">
+
+<label for="newsButton">  </label>
+
+
+<nav>
+
+<input type="checkbox" id="menuButton">
+
+<label for="menuButton">  </label>
+
+
+<ul>
+
+<li><a href="index.php"> Acasa</a></li>
+<li><a href="p1.php"> Programari</a></li>
+<li><a href="p2.php"> Cauta</a></li>
+<li><a href="p3.php">Cont</a></li>
+<li><a href="p4.php"> Contact</a></li>
+
+</ul>
+
+
+</nav>
+
+<aside>
+
+<h2>Autentificare </h2>
+ <form action="p2.php" method="post">
+  <input type="text" name="login_username" placeholder="Nume"><br>
+  <input type="password" name="login_password" placeholder="Parola"><br><br>
+  <button type="submit" name="login_submit">Log In</button>
+</form> 
+<p><h2>Inregistrare</h2></p>
+ <form action="p2.php" method="post">
+  <input type="text" name="register_username" placeholder="Nume"><br>
+  <input type="text" name="register_userpname" placeholder="Prenume"><br>
+  <input type="text" name="register_email" placeholder="Telefon"><br>
+  <input type="text" name="register_telefon" placeholder="Email"><br>
+  <input type="password" name="register_password" placeholder="Parola"><br>
+  <input type="password" name="register_passwordd" placeholder="Confirm Parola"><br><br>
+  <button type="submit" name="register_submit">Register</button>
+</form>
+
+</aside>
+
+<section>
+<br><br>
+<form action="p2.php" method="post">
+  <input type="text" name="nume_magazin" placeholder="Numele magazinului"><br>
+  <input type="submit" name="caut_magazin_nume" value="Submit">
+</form> 
+
+<form action="p2.php" method="post">
+  <input type="text" name="id_magazin" placeholder="Id-ul magazinului"><br>
+  <input type="submit" name="caut_magazin_id" value="Submit">
+</form> 
+<?php
+if(isset($_POST['caut_magazin_nume']))
+{
+	Cautmagazinnume();
+}
+
+if(isset($_POST['caut_magazin_id']))
+{
+	Cautmagazinid();
+}
+
+
+?>
+</section>
+
+</main>
+
+</body>
+
+
+</html>
+
+ <?php
 
 if(isset($_POST['login_submit']))
 {
@@ -181,15 +235,5 @@ else
 if(isset($_POST['register_submit']))
 {
 	Register();
-}
-else
-if(isset($_POST['register_submit']))
-{
-	Cautmagazinnume();
-}
-else
-if(isset($_POST['register_submit']))
-{
-	Cautmagazinid();
 }
 ?>
--- ce am facut eu la site/p22.php
+++ ce am facut eu la site/p22.php
@@ -3,6 +3,143 @@
  if($_SESSION['user'] == NULL) {
   header('Location: index.php'); 
                                     } 
+									
+		$conn = oci_connect('STUDENT','STUDENT','localhost/XE') or die;
+
+
+ function ChangePass()
+ {
+	 global $conn;
+	 $id = $_SESSION['id'];
+	 echo "<p>" . $id . "</p>"; 
+	 $old_pass = $_POST['oldpassword'];
+	 $new_pass = $_POST['newpassword'];
+	 $sql = 'BEGIN schimbare_parola(:id, :old_pass, :new_pass); END;';
+	 $stmt = oci_parse($conn, $sql);
+	 oci_bind_by_name($stmt,':id',$id,32);
+	 oci_bind_by_name($stmt,':old_pass',$old_pass,32);
+	 oci_bind_by_name($stmt,':new_pass',$new_pass,32);
+	 oci_execute($stmt);
+ }
+							
+									
+function Cautmagazinnume()
+{
+	global $conn;
+	$nume_magazin = $_POST['nume_magazin'];
+	$sql = "select id,tip from servicii where id=(select id_serviciu from furnizori where nume='".$nume_magazin."' and rownum=1)";
+	$stmt = oci_parse($conn,$sql);
+	$r = oci_execute($stmt);
+	
+	
+	
+	echo "<table border='1'>\n";
+$ncols = oci_num_fields($stmt);
+echo "<tr>\n";
+for ($i = 1; $i <= $ncols; ++$i) {
+    $colname = oci_field_name($stmt, $i);
+    echo "  <th><b>".htmlspecialchars($colname,ENT_QUOTES|ENT_SUBSTITUTE)."</b></th>\n";
+}
+echo "</tr>\n";
+ 
+	
+while (($row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
+    echo "<tr>\n";
+    foreach ($row as $item) {
+        echo "<td>";
+        echo $item!==null?htmlspecialchars($item, ENT_QUOTES|ENT_SUBSTITUTE):"&nbsp;";
+        echo "</td>\n";
+    }
+}
+echo "</table>\n";
+}
+function Cautmagazinid()
+{
+	global $conn;
+	$id_magazin = $_POST['id_magazin'];
+   $sql = "select id,tip from servicii where id=(select id_serviciu from furnizori where id=".$id_magazin." and rownum=1)";
+	$stmt = oci_parse($conn,$sql);
+	$r = oci_execute($stmt);
+	
+	
+	
+	echo "<table border='1'>\n";
+$ncols = oci_num_fields($stmt);
+echo "<tr>\n";
+for ($i = 1; $i <= $ncols; ++$i) {
+    $colname = oci_field_name($stmt, $i);
+    echo "  <th><b>".htmlspecialchars($colname,ENT_QUOTES|ENT_SUBSTITUTE)."</b></th>\n";
+}
+echo "</tr>\n";
+ 
+	
+while (($row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
+    echo "<tr>\n";
+    foreach ($row as $item) {
+        echo "<td>";
+        echo $item!==null?htmlspecialchars($item, ENT_QUOTES|ENT_SUBSTITUTE):"&nbsp;";
+        echo "</td>\n";
+    }
+}
+echo "</table>\n";
+}
+
+
+function Afiseaza_review()
+{
+	global $conn;
+$afiseaza_ids = $_POST['afiseaza_ids'];
+
+}
+
+function Afiseaza_review_cenzurat()
+{
+	global $conn;
+$afiseaza_cenzurat_ids = $_POST['afiseaza_cenzurat_ids'];
+
+}
+
+function Verifica_ora_libera()
+{
+	global $conn;
+			$verific_idm = $_POST['verific_idm'];
+$verific_ids = $_POST['verific_ids'];
+$ora = $_POST['ora'];
+
+}
+
+function Adauga_review()
+{
+	global $conn;
+			$adaug_idm = $_POST['adaug_idm'];
+$adaug_ids = $_POST['adaug_ids'];
+$adaug_text = $_POST['adaug_text'];
+
+}
+
+function Sterge_review()
+{
+	global $conn;
+			$sterge_idr = $_POST['sterge_idr'];
+}
+
+function Vezi_review()
+{
+	global $conn;
+}
+
+function Procentaj_clienti()
+{
+	global $conn;
+}
+
+function Procentaj_clienti_magazin()
+{
+	global $conn;
+			$procentaj_idm = $_POST['procentaj_idm'];
+$procentaj_ids = $_POST['procentaj_ids'];
+
+}
 ?>
 
 <!DOCTYPE html>
@@ -54,9 +191,15 @@
   <input type="submit" name="pass_chng_submit" value="Submit">
 </form> 
 <br><br>
- <form action="indexx.php" method="post">
-  <input type="submit"  name="submit" value="Log out">
-</form>
+<form action="p22.php" method="post">
+  <input type="text" name="nume_magazin" placeholder="Numele magazinului"><br>
+  <input type="submit" name="caut_magazin_nume" value="Submit">
+</form> 
+
+<form action="p22.php" method="post">
+  <input type="text" name="id_magazin" placeholder="Id-ul magazinului"><br>
+  <input type="submit" name="caut_magazin_id" value="Submit">
+</form> 
 
 
 </aside>
@@ -64,21 +207,38 @@
 <section>
 <h2> Cauta magazin</h2>
 <form action="p22.php" method="post">
-  <input type="text" name="caut_idm" placeholder="Numele magazinului"><br><br>
-  <input type="text" name="caut_ids" placeholder="Tipul maganului"><br><br>
-  <input type="submit" name="cautmagazin" value="Submit">
+  <input type="text" name="nume_magazin" placeholder="Numele magazinului"><br>
+  <input type="submit" name="caut_magazin_nume" value="Submit">
 </form> 
+
+<form action="p22.php" method="post">
+  <input type="text" name="id_magazin" placeholder="Id-ul magazinului"><br>
+  <input type="submit" name="caut_magazin_id" value="Submit">
+</form> 
+
+
+<?php
+if(isset($_POST['caut_magazin_nume']))
+{
+	Cautmagazinnume();
+}
+
+if(isset($_POST['caut_magazin_id']))
+{
+	Cautmagazinid();
+}
+
+
+?>
 
 <h2>Afiseaza review</h2>
  <form action="p22.php" method="post">
-  <input type="user" name="afiseaza_idm" placeholder="Id magazin"><br><br>
     <input type="user" name="afiseaza_ids" placeholder="Id serviciu"><br><br>
   <input type="submit" name="afiseazareview" value="Submit">
 </form>
 
 <h2>Afiseaza review cenzurat</h2>
  <form action="p22.php" method="post">
-  <input type="user" name="afiseaza_cenzurat_idm" placeholder="Id magazin"><br><br>
     <input type="user" name="afiseaza_cenzurat_ids" placeholder="Id serviciu"><br><br>
   <input type="submit" name="afiseazareviewcenzurat" value="Submit">
 </form>
@@ -134,96 +294,8 @@
 
 </html>
 <?php
- $conn = oci_connect('STUDENT','STUDENT','localhost/XE') or die;
+ 
 
-
- function ChangePass()
- {
-	 global $conn;
-	 $id = $_SESSION['id'];
-	 echo "<p>" . $id . "</p>"; 
-	 $old_pass = $_POST['oldpassword'];
-	 $new_pass = $_POST['newpassword'];
-	 $sql = 'BEGIN schimbare_parola(:id, :old_pass, :new_pass); END;';
-	 $stmt = oci_parse($conn, $sql);
-	 oci_bind_by_name($stmt,':id',$id,32);
-	 oci_bind_by_name($stmt,':old_pass',$old_pass,32);
-	 oci_bind_by_name($stmt,':new_pass',$new_pass,32);
-	 oci_execute($stmt);
- }
-
-function Caut_magazin()
-{
-	global $conn;
-	$caut_idm = $_POST['caut_idm'];
-$caut_ids = $_POST['caut_ids'];
-
-	
-}
-
-function Afiseaza_review()
-{
-	global $conn;
-		$afiseaza_idm = $_POST['afiseaza_idm'];
-$afiseaza_ids = $_POST['afiseaza_ids'];
-
-}
-
-function Afiseaza_review_cenzurat()
-{
-	global $conn;
-			$afiseaza_cenzurat_idm = $_POST['afiseaza_cenzurat_idm'];
-$afiseaza_cenzurat_ids = $_POST['afiseaza_cenzurat_ids'];
-
-}
-
-function Verifica_ora_libera()
-{
-	global $conn;
-			$verific_idm = $_POST['verific_idm'];
-$verific_ids = $_POST['verific_ids'];
-$ora = $_POST['ora'];
-
-}
-
-function Adauga_review()
-{
-	global $conn;
-			$adaug_idm = $_POST['adaug_idm'];
-$adaug_ids = $_POST['adaug_ids'];
-$adaug_text = $_POST['adaug_text'];
-
-}
-
-function Sterge_review()
-{
-	global $conn;
-			$sterge_idr = $_POST['sterge_idr'];
-}
-
-function Vezi_review()
-{
-	global $conn;
-}
-
-function Procentaj_clienti()
-{
-	global $conn;
-}
-
-function Procentaj_clienti_magazin()
-{
-	global $conn;
-			$procentaj_idm = $_POST['procentaj_idm'];
-$procentaj_ids = $_POST['procentaj_ids'];
-
-}
-
-if(isset($_POST['cautmagazin']))
-{
-	Caut_magazin();
-}
-else
 if(isset($_POST['afiseazareview']))
 {
 	Afiseaza_review();
@@ -268,4 +340,5 @@ if (isset($_POST['pass_chng_submit']))
 {
 	ChangePass();
 }
+
 ?>
