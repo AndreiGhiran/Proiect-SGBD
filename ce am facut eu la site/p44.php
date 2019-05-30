31ed2d4adae53a912ff69edda406a609abd13f11
andreicaloian <48419225+andreicaloian@users.noreply.github.com>
2019-05-30 07:04:31 +0300

    Add files via upload

--- ce am facut eu la site/p44.php
+++ ce am facut eu la site/p44.php
@@ -65,6 +65,31 @@
 <br>
 <h2>Caloian Andrei George -andrei.caloian@uaic.ro<br>
 Ghiran Andrei -andrei.ghiran@uaic.ro</h2>
+
+<br><br>
+ <form action="test_oracle.php" method="post">
+  <input type="submit"  name="test_oracle" value="Test oracle">
+</form>
+
+
+<br><br>
+ <form action="p44.php" method="post">
+  <input type="submit"  name="trustpropiu" value="Calculeaza propiul trust factor">
+</form>
+
+
+<br><br>
+ <form action="p44.php" method="post">
+  <input type="submit"  name="calculeazatrustfactor" value="Calculeaza trust factor la toata lumea">
+</form>
+
+<br><br>
+ <form action="p44.php" method="post">
+  <input type="password" name="id_client" placeholder="Id client"><br><br>
+  <input type="submit"  name="calculeazatrustfactorsingle" value="Calculeaza trust factor unui client">
+</form>
+
+
 </section>
 
 </main>
@@ -89,8 +114,49 @@ Ghiran Andrei -andrei.ghiran@uaic.ro</h2>
 	 oci_execute($stmt);
  }
 
+function Trustpropiu(){
+global $conn;
+	 $id = $_SESSION['id'];
+		$sql = 'BEGIN calculeaza_trust_factor_single(:id); END;';
+		$stmt = oci_parse($conn,$sql);
+		 oci_bind_by_name($stmt,':id',$id,32);
+		oci_execute($stmt);
+
+}
+
+function TrustFactor(){
+		global $conn;
+		$sql = 'BEGIN CALCULEAZA_TRUST_FACTOR_ALL; END;';
+		$stmt = oci_parse($conn,$sql);
+		oci_execute($stmt);
+	
+}
+
+function Calculeazatrustfactorsingle(){
+	global $conn;
+	$id=$_POST['id_client'];
+			$sql = 'BEGIN calculeaza_trust_factor_single(:id); END;';
+		$stmt = oci_parse($conn,$sql);
+		 oci_bind_by_name($stmt,':id',$id,32);
+		oci_execute($stmt);
+	
+}
+
 if (isset($_POST['pass_chng_submit']))
 {
 	ChangePass();
 }
+if (isset($_POST['calculeazatrustfactor']))
+{
+	Calculeazatrustfactor();
+}
+if (isset($_POST['calculeazatrustfactorsingle']))
+{
+	Calculeazatrustfactorsingle();
+}
+if (isset($_POST['trustpropiu']))
+{
+	Trustpropiu();
+}
+
 ?>
